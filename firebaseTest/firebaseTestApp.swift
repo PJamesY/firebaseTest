4682de89864b08cb92402f02685fa66335ec6fc9
james <junyoungcat@gmail.com>
2021-11-16 22:38:25 +0900

    Initial Commit

---
+++ firebaseTest/firebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseTestApp.swift
+//  firebaseTest
+//
+//  Created by james on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct firebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
