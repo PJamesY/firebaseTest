d7b43ee64840b94e0a437fede5d538ffe04b6b0e
james <junyoungcat@gmail.com>
2021-11-18 22:26:26 +0900

    Create ViewModel

---
+++ firebaseTest/ViewModels/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  firebaseTest
+//
+//  Created by james on 2021/11/18.
+//
+
+import Foundation
+
+class ViewModel: ObservableObject {
+    @Published var list = ["Apple", "Carrot", "Banana"]
+}
