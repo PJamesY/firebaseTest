d6dc7746ec44e402beabf06818788110a6108378
james <junyoungcat@gmail.com>
2021-11-18 22:29:03 +0900

    Apply view model list

--- firebaseTest/Views/ContentView.swift
+++ firebaseTest/Views/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var model = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!!")
-            .padding()
+        List(model.list, id: \.self) { item in
+            Text(item)
+            
+        }
     }
 }
 
