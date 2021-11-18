915e9946bfa03001bd384c40127ab2b1d0e70955
james <junyoungcat@gmail.com>
2021-11-18 22:32:09 +0900

    Create model todo

---
+++ firebaseTest/Models/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  firebaseTest
+//
+//  Created by james on 2021/11/18.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id: String
+    var name: String
+    var note: String
+}
