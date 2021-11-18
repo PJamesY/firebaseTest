80104986ebd911ddafb3b036b2b96a5252bf7176
james <junyoungcat@gmail.com>
2021-11-18 22:47:37 +0900

    Install fireStore and access to firestore DB

--- Podfile
+++ Podfile
@@ -9,5 +9,5 @@ target 'firebaseTest' do
   
   pod 'Firebase/Core'
   pod 'Firebase/Database'
-
+  pod 'Firebase/Firestore'
 end
--- firebaseTest/ViewModels/ViewModel.swift
+++ firebaseTest/ViewModels/ViewModel.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 class ViewModel: ObservableObject {
-    @Published var list = ["Apple", "Carrot", "Banana"]
+    @Published var list = [Todo]()
+    
+    func getData()  {
+        // Get ad reference to the database
+        let db = Firestore.firestore()
+        
+        // Read the documents at a specific path
+        db.collection("todos").getDocuments { snapshot, error in
+            // check for errors
+            if error == nil {
+                // No errors
+                if let snapshot = snapshot {
+                    
+                    // Update the list property in the main thread
+                    DispatchQueue.main.async {
+                        // Get all the documents and create Todos
+                        self.list = snapshot.documents.map { d in
+                            
+                            // Create a Todo item for each document returned
+                            return Todo(id: d.documentID,
+                                        name: d["name"] as? String ?? "",
+                                        note: d["notes"] as? String ?? "")
+                        }
+                    }
+                    
+                }
+            }
+            else {
+                // Handle the error
+            }
+        }
+        
+    }
 }
