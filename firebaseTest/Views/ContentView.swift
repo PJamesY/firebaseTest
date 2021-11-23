168b012f69537c8ab5c163e02cd17e9beabc335d
james <junyoungcat@gmail.com>
2021-11-23 22:17:45 +0900

    Add text filed ui for add item

--- firebaseTest/Views/ContentView.swift
+++ firebaseTest/Views/ContentView.swift
@@ -11,11 +11,42 @@ struct ContentView: View {
     
     @ObservedObject var model = ViewModel()
     
+    @State var name = ""
+    @State var notes = ""
+    
     var body: some View {
-        List(model.list) { item in
-            Text(item.name)
+        
+        VStack {
+            List(model.list) { item in
+                Text(item.name)
+                
+            }
+            
+            Divider()
+            
+            VStack(spacing: 5) {
+                TextField("Name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Notes", text: $notes)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    // call add data
+                    model.addData(name: name, notes: notes)
+                    
+                    // clear the text fields
+                    name = ""
+                    notes = ""
+                    
+                }, label: {
+                    Text("Add Todo Item")
+                })
+            }
+            .padding()
             
         }
+        
+        
     }
     
     init() {
